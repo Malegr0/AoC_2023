b0767b3aa8513fb34db381e778266fd60c4efcd5
Malegr0 <schoening.andreas@gmx.de>
2023-12-14 01:18:10 +0100

    Day 1 done

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import day1
 
 
-def print_hi(name):
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
-
-
 if __name__ == '__main__':
-    print_hi('PyCharm')
-    day1.test()
+    day1.run()
